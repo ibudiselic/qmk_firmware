369618a3b1bbd3c1968c0fc7936f6491ba459601
Ivan Budiselic <ibudiselic@gmail.com>
2021-10-22 13:28:16 +0200

    Remove a few unused keys.

--- keyboards/moonlander/keymaps/ibudiselic/keymap.c
+++ keyboards/moonlander/keymaps/ibudiselic/keymap.c
@@ -35,20 +35,20 @@ enum tap_dance_codes {
 #define _ KC_NO
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [IBUD_LAY_BASE] = LAYOUT_moonlander(
-       KC_AT,               KC_1,                KC_2,                KC_3,                KC_4,                KC_5,                KC_F4,                     KC_F5,               KC_6,                KC_7,                KC_8,                KC_9,                KC_0,                KC_MINUS,
+       KC_AT,               KC_1,                KC_2,                KC_3,                KC_4,                KC_5,                KC_F4,                     KC_F5,               KC_6,                KC_7,                KC_8,                KC_9,                KC_0,                _,
        KC_TAB,              KC_Q,                KC_W,                KC_E,                KC_R,                KC_T,                KC_HOME,                   KC_PGUP,             KC_Y,                KC_U,                KC_I,                KC_O,                KC_P,                KC_BSLASH,
        KC_ESCAPE,           IBLT(CTRL, KC_A),    IBLT(FKEYS, KC_S),   LSFT_T(KC_D),        IBLT(NUM, KC_F),     KC_G,                KC_END,                    KC_PGDOWN,           KC_H,                KC_J,                RSFT_T(KC_K),        KC_L,                KC_SCOLON,           RGUI_T(KC_QUOTE),
-       KC_GRAVE,            KC_Z,                KC_X,                KC_C,                KC_V,                KC_B,                                                                KC_N,                KC_M,                KC_COMMA,            KC_DOT,              KC_SLASH,            KC_EQUAL,
-       _,                   KC_F3,               KC_F2,               LSFT(KC_LALT),       KC_LALT,             TD(DANCE_0),                                                         TD(DANCE_2),         TD(DANCE_1),         KC_F1,               KC_LBRACKET,         KC_RBRACKET,         _,
+       KC_GRAVE,            KC_Z,                KC_X,                KC_C,                KC_V,                KC_B,                                                                KC_N,                KC_M,                KC_COMMA,            KC_DOT,              KC_SLASH,            _,
+       _,                   _,                   _,                   LSFT(KC_LALT),       KC_LALT,             TD(DANCE_0),                                                         TD(DANCE_2),         TD(DANCE_1),         _,                   _,                   _,                   _,
                                                                       LCTL_T(KC_SPACE),    C_S_T(KC_BSPACE),    LCA_T(KC_ENTER),                                                     KC_DELETE,           KC_BSPACE,           IBLT(SYM, KC_ENTER)
     ),
 
     [IBUD_LAY_GAME] = LAYOUT_moonlander(
-       KC_F6,               KC_1,                KC_2,                KC_3,                KC_4,                KC_5,                KC_F4,                     KC_F5,               KC_6,                KC_7,                KC_8,                KC_9,                KC_0,                KC_MINUS,
+       KC_F6,               KC_1,                KC_2,                KC_3,                KC_4,                KC_5,                KC_F4,                     KC_F5,               KC_6,                KC_7,                KC_8,                KC_9,                KC_0,                _,
        KC_TAB,              KC_Q,                KC_W,                KC_E,                KC_R,                KC_T,                KC_HOME,                   KC_PGUP,             KC_Y,                KC_U,                KC_I,                KC_O,                KC_P,                KC_BSLASH,
        KC_ESCAPE,           KC_A,                KC_S,                KC_D,                KC_F,                KC_G,                KC_END,                    KC_PGDOWN,           KC_H,                KC_J,                RSFT_T(KC_K),        KC_L,                KC_SCOLON,           RGUI_T(KC_QUOTE),
        KC_LSHIFT,           KC_Z,                KC_X,                KC_C,                KC_V,                KC_B,                                                                KC_N,                KC_M,                KC_COMMA,            KC_DOT,              KC_SLASH,            KC_RSHIFT,
-       KC_F7,               KC_F3,               KC_F2,               LCTL(KC_LALT),       KC_LALT,             KC_F8,                                                               TD(DANCE_3),         _,                   _,                   KC_LBRACKET,         KC_RBRACKET,         KC_RCTRL,
+       KC_F7,               KC_F3,               KC_F2,               LCTL(KC_LALT),       KC_LALT,             KC_F8,                                                               TD(DANCE_3),         _,                   _,                   _,                   _,                   KC_RCTRL,
                                                                       KC_SPACE,            KC_F2,               KC_F1,                                                               KC_DELETE,           KC_BSPACE,           KC_ENTER
     ),
 
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_DQUO,             KC_COLN,             KC_EQUAL,            KC_PLUS,             KC_LPRN,             KC_ASTR,             KC_ENTER,                  _,                   KC_SLASH,            KC_RPRN,             KC_MINUS,            KC_UNDS,             KC_SCOLON,           KC_QUOTE,
        KC_GRAVE,            KC_EXLM,             KC_LABK,             KC_LBRACKET,         KC_LCBR,             KC_AMPR,                                                             KC_PIPE,             KC_RCBR,             KC_RBRACKET,         KC_RABK,             KC_QUES,             KC_CIRC,
        KC_PERC,             _,                   _,                   _,                   _,                   _,                                                                   _,                   _,                   _,                   _,                   _,                   KC_TILD,
-                                                                      KC_SPACE,            KC_DLR,              KC_HASH,                                                             _,                   _,                   _
+                                                                      KC_SPACE,            _,                   _,                                                                   _,                   _,                   _
     ),
 };
 #undef _
